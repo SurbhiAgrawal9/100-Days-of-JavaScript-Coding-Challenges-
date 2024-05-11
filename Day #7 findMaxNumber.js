fba5a14482cabe00b629e37e30a92849bae5b1ba
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2024-05-12 01:45:30 +0530

    Create Day #7 findMaxNumber.js

---
+++ Day #7 findMaxNumber.js
@@ -0,0 +1,21 @@
+//for find maxnumber for array
+
+function findMax(arr){
+    
+    return Math.max(...arr)  
+}
+console.log(findMax([2,4,6,9,4]))  //9
+
+
+
+
+
+
+
+//for numbers (non-aaray)
+
+function findMax(){
+ 
+ return Math.max(2,4,6,9,4)   
+}
+console.log(findMax())    //9
