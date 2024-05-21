23aefeae84a782ff35cf47480433c278d923822a
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2024-05-22 01:57:44 +0530

    Update Day #16 findMin.js

--- Day #16 findMin.js
+++ Day #16 findMin.js
@@ -18,3 +18,23 @@ console.log(findMin([20,2,5,10]))
 
 //trial  
 console.log(Math.min(...[20,2,5,-3,10,100,1,0]))    //0
+
+
+
+
+
+
+// max value of array is also same as min 
+
+function findMax(arr){
+    
+//   arr =  arr.sort((a,b) => {
+//         if(a > b) return -1
+//     })
+//     return arr[0]
+return Math.max(...arr)
+}
+
+console.log(findMax([20,4,30,2]))
+
+console.log(Math.max(...[20,100,3,0]))
